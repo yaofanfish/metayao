c3f63634c080ad84d4e3ec6f9b4cf8986ab0ab34
yaofanfish <yaofanfish@gmail.com>
2026-04-24 18:41:16 +0100

    EDATE

---
+++ EDATE.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys, os, subprocess
+import json, argparse
+
+o_IQHIGH = ["Yao", "Yao"]
+o_IQLOW = ["Keita", "Keita"]
+
+def main(before, after, exclude):
+	EXCLUDE = ["EDATE.py"]
+	EXCLUDE += exclude
+	
+	files = os.listdir()
+	
+	for file in files:
+		if file in EXCLUDE: break
+	with open(file) as f:
+		contents = f.read()
+	for i in range(len(before)):
+		for avant in before[i]:
+			contents = contents.replace(avant, after[i])
+	with open(file, "w") as f:
+		f.write(contents)
+
+p = argparse.ArgumentParser("Change the names")
+
+p.add_argument("-a", "--aiq-high", help="The person with high IQ", default="Yao")
+p.add_argument("-b", "--bum", help="The person with low IQ", default="Keita")
+p.add_argument("-oa", "--oa", help="original a", default=o_IQHIGH)
+p.add_argument("-ob", "--ob", help="original b", default=o_IQLOW)
+p.add_argument("-x", "--exclude", help="exclude files (csv)", default=[], type=lambda x: x.split(","))
+
+args = p.parse_args()
+
+main([args.oa, args.ob], [args.aiq_high, args.bum], args.exclude)
+
